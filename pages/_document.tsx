db230aa68ce98d9dd39e88fe31215047dc286a49
mvn-tinhle-dn <tinh.le@monstar-lab.com>
2022-08-23 15:00:56 +0700

    Test: fix fontawesome

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,10 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.15.0/css/all.css"
+          ></link>
         </Head>
         <body>
           <Main />
